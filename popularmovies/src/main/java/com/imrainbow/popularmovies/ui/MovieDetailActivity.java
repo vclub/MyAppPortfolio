bac0f51d59141f1f2cae243d9da3d0f87712ba29
李宾 <libin@cnlive.com>
2015-09-21 18:08:42 +0800

    check favorite

--- popularmovies/src/main/java/com/imrainbow/popularmovies/ui/MovieDetailActivity.java
+++ popularmovies/src/main/java/com/imrainbow/popularmovies/ui/MovieDetailActivity.java
@@ -1,6 +1,12 @@
 package com.imrainbow.popularmovies.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.imrainbow.popularmovies.R;
 import com.imrainbow.popularmovies.dao.MovieEntity;
@@ -14,6 +20,7 @@ public class MovieDetailActivity extends BackBaseActivity {
 
 
     private MovieEntity mCurrentMovie;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,29 @@ public class MovieDetailActivity extends BackBaseActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.container,
                     MovieDetailFragment.newInstance(mCurrentMovie)).commit();
         }
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("http://youtu.be/v/dddd"));
+        setShareIntent(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_movie_detail, menu);
 
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+
+        mShareActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(item);
+
+        return true;
+    }
+
+    private void setShareIntent(Intent shareIntent){
+        if (mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 }
