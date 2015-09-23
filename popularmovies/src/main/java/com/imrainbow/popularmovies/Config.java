695910ab3bfc2706134e92ca78f1b084ca2b32e0
vclub <vclub@126.com>
2015-09-23 21:40:49 +0800

    fix key

--- popularmovies/src/main/java/com/imrainbow/popularmovies/Config.java
+++ popularmovies/src/main/java/com/imrainbow/popularmovies/Config.java
@@ -5,7 +5,7 @@ package com.imrainbow.popularmovies;
  */
 public class Config {
 
-    public static String THE_MOVIE_DB_API_KEY = "4ebd4218ff55a7efbeeb1ee52bd59a3e";
+    public static String THE_MOVIE_DB_API_KEY = "Please replace this value to your api key";
     public static String THE_MOVIE_DB_BASE_URL = "http://api.themoviedb.org/3";
 
     public static final String SORT_VALUE_KEY = "sort_key";
